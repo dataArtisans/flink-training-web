d3bf3d088504b189e051c5feb13935545517de54
Ufuk Celebi <uce@apache.org>
2015-05-19 14:23:21 +0200

    Initialize Jekyll

---
+++ web/preview.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# Serves the page locally at http://localhost:4000. The result of this build
+# cannot be used online, because the baseurl is not set.
+
+jekyll s --baseurl ""
